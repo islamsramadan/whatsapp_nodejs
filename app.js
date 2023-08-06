983164d83db7d8f688a5b582f1c5ade7b25a4a32
islam ramadan <islamsramadan@outlook.com>
2023-08-06 14:16:58 +0300

    webhook sendStatus

--- app.js
+++ app.js
@@ -77,34 +77,34 @@ app.post('/webhook', (req, res) => {
       console.log('from', from);
       console.log('msgBody', msgBody);
 
-      // axios({
-      //   method: 'post',
-      //   url: `https://graph.facebook.com/v17.0/${phoneNumberID}/messages`,
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //     Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
-      //   },
-      //   data: JSON.stringify({
-      //     messaging_product: 'whatsapp',
-      //     recipient_type: 'individual',
-      //     to: from,
-      //     type: 'text',
-      //     text: {
-      //       preview_url: false,
-      //       body: "hello it's me and this is your message: " + msgBody,
-      //     },
-      //   }),
-      // })
-      //   .then((response) => {
-      //     console.log('Response ==============', JSON.stringify(response.data));
-      //   })
-      //   .catch((error) => {
-      //     console.log(error);
-      //   });
+      axios({
+        method: 'post',
+        url: `https://graph.facebook.com/v17.0/${phoneNumberID}/messages`,
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+        },
+        data: JSON.stringify({
+          messaging_product: 'whatsapp',
+          recipient_type: 'individual',
+          to: from,
+          type: 'text',
+          text: {
+            preview_url: false,
+            body: "hello it's me and this is your message: " + msgBody,
+          },
+        }),
+      })
+        .then((response) => {
+          console.log('Response ==============', JSON.stringify(response.data));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
 
-      res.status(200).end;
+      res.sendStatus(200);
     } else {
-      res.status(404).end;
+      res.sendStatus(404);
     }
   }
 });
