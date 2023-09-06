92954c082fe1be39434ed488534277dd53b83e8a
islam ramadan <islamsramadan@outlook.com>
2023-09-06 17:01:43 +0300

    update error message

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -8,8 +8,9 @@ const handleCastErrorDB = (err) => {
 };
 
 const handleDuplicateFieldsDB = (err) => {
-  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-  console.log(value);
+  const value = err.message.match(/(["'])(\\?.)*?\1/)[0];
+  // console.log(value);
+  // console.log(err);
 
   const message = `Duplicate field value: ${value}. Please use another value!`;
   return new AppError(message, 400);
