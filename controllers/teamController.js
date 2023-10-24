a59eef53b4149715c5094a5338b30916121e4e90
islam ramadan <islamsramadan@outlook.com>
2023-10-24 16:22:57 +0300

    team populate

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -138,7 +138,8 @@ exports.getTeam = catchAsync(async (req, res, next) => {
     .populate('supervisor', 'firstName lastName photo')
     .populate('users', 'firstName lastName photo')
     .populate('creator', 'firstName lastName photo')
-    .populate('answersSets');
+    .populate('answersSets')
+    .populate('serviceHours', 'name');
 
   if (!team) {
     return next(new AppError('No team found with that ID!'));
@@ -171,27 +172,7 @@ exports.updateTeam = catchAsync(async (req, res, next) => {
       { photo: req.file.filename },
       { new: true, runValidators: true }
     );
-  }
-
-  // // Updating team to default team
-  // else if (req.body.default === true) {
-  //   const team = await Team.findById(req.params.id);
-  //   if (!team) {
-  //     return next(new AppError('No team found with that ID!', 404));
-  //   }
-
-  //   //remove default from the other default team
-  //   await Team.findOneAndUpdate(
-  //     { default: true },
-  //     { default: false },
-  //     { new: true, runValidators: true }
-  //   );
-
-  //   //update the new default team
-  //   team.default = true;
-  //   updatedTeam = await team.save();
-  // }
-  else {
+  } else {
     // Updating team by the provided in the body
     if (!req.body.supervisor || !(await User.findById(req.body.supervisor))) {
       return next(new AppError('Team supervisor is required!', 400));
