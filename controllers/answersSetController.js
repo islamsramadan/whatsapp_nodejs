6f445ce621fd90562ff0a572765c7270a7186c0c
islam ramadan <islamsramadan@outlook.com>
2023-10-30 12:51:20 +0200

    private answers

--- controllers/answersSetController.js
+++ controllers/answersSetController.js
@@ -1,6 +1,7 @@
 const catchAsync = require('../utils/catchAsync');
 const AnswersSet = require('../models/answersSetModel');
 const AppError = require('../utils/appError');
+const User = require('../models/userModel');
 
 exports.getAllAnswersSet = catchAsync(async (req, res, next) => {
   const answersSets = await AnswersSet.find()
