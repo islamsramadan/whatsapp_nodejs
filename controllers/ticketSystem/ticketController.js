724c48d9676125e5e949d2491b127719a8067a08
islam ramadan <islamsramadan@outlook.com>
2024-10-03 14:44:39 +0300

    permission updates

--- controllers/ticketSystem/ticketController.js
+++ controllers/ticketSystem/ticketController.js
@@ -973,7 +973,7 @@ exports.updateTicketInfo = catchAsync(async (req, res, next) => {
 
   let updatedBody = {};
   if (ticket.status.category !== 'solved') {
-    updatedBody = filterObj(req.body, 'priority', 'category');
+    updatedBody = filterObj(req.body, 'requestType', 'priority', 'category');
   }
 
   // ----------> Status validation
