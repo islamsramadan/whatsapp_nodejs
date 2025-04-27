6ba0662758d7938330b27a150cd6c1b1e11da29b
islam ramadan <islamsramadan@outlook.com>
2025-04-27 21:46:25 +0300

    update buttons templates

--- models/messageModel.js
+++ models/messageModel.js
@@ -137,12 +137,15 @@ const messageSchema = new mongoose.Schema(
             {
               type: {
                 type: String,
-                enum: ['QUICK_REPLY', 'URL'],
+                enum: ['QUICK_REPLY', 'URL', 'PHONE_NUMBER'],
               },
               text: {
                 type: String,
               },
               url: {
+                type: String,
+              },
+              phone_number: {
                 type: String,
               },
             },
